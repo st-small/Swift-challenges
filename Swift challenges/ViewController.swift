c488d7cf9fb5cf3036337f2223e50b390b00d51d
Станислав Шияновский <st-small@rambler.ru>
2020-03-28 19:24:55 +0200

    Initial Commit

---
+++ Swift challenges/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift challenges
+//
+//  Created by Stanly Shiyanovskiy on 28.03.2020.
+//  Copyright © 2020 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
